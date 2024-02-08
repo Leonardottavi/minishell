9f979a461caccc4308d631095dfd0c45b34017ca
Lottavi <leonardottavi02@gmail.com>
2024-02-07 16:07:02 -0800

    Update user authentication logic

---
+++ src/minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 11:10:20 by lottavi           #+#    #+#             */
+/*   Updated: 2024/02/07 15:58:44 by lottavi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "../lib/libft.h"
+
+//terminal color
+#define RESET   "\033[0m"
+#define BOLD    "\033[1m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
