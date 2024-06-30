d2645538762b275c4f0e3b24422d269a9c2e05d5
lottavi <leonardottavi02@gmail.com>
2024-06-30 20:24:05 +0200

    minishell.h norm fatte

--- src/minishell.h
+++ src/minishell.h
@@ -6,50 +6,49 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 12:22:17 by lottavi           #+#    #+#             */
-/*   Updated: 2024/06/30 20:07:33 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/06/30 20:23:29 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <fcntl.h>
-#include "../lib/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <fcntl.h>
+# include "../lib/libft.h"
 
-#define MAX_BUFFER_SIZE 80 // buffer to hold user input
-#define BUILTIN_COMMANDS 8 // number of bult-in commands
+# define MAX_BUFFER_SIZE 80 // buffer to hold user input
+# define BUILTIN_COMMANDS 8 // number of bult-in commands
 
-extern char g_path[1024];
-extern char* g_builtin_commands[];
-extern int (*g_builtin_functions[BUILTIN_COMMANDS])(char**);
+extern char		g_path[1024];
+extern char		*g_builtin_commands[];
+extern	int		*(g_builtin_functions[BUILTIN_COMMANDS])(char**);
 
-int builtin_exit(char** args);
-int builtin_cd(char** args);
-int builtin_help(char** args);
-int builtin_history(char** args);
-int builtin_env(char** args);
-int builtin_pwd(char** args);
-int builtin_echo(char** args);
-int builtin_unset(char** args);
-int builtin_cat(char** args);
+int		builtin_exit(char **args);
+int		builtin_cd(char **args);
+int		builtin_help(char **args);
+int		builtin_history(char **args);
+int		builtin_env(char **args);
+int		builtin_pwd(char **args);
+int		builtin_echo(char **args);
+int		builtin_unset(char **args);
+int		builtin_cat(char **args);
 
 // function shell
-void sigint_handler();
-void save_history(char* input);
-char* get_input(char* buffer);
-char** get_args(char* input, char** args);
-
+void	sigint_handler(void);
+void	save_history(char *input);
+char	*get_input(char *buffer);
+char	**get_args(char *input, char **args);
 
 //function executers
-int execute_without_pipe(char** args);
-int execute_with_pipe(char** args);
-int execute(char** args);
+int		execute_without_pipe(char **args);
+int		execute_with_pipe(char **args);
+int		execute(char **args);
 
 #endif
