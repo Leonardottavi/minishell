eec6ecb239b7827aef0d808de260f35fecb20e5e
Leonardo Ottavi <leonardottavi02@gmail.com>
2024-07-05 14:31:11 +0200

    Fix readline prompt in shell.c

--- src/shell.c
+++ src/shell.c
@@ -6,7 +6,7 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 12:24:47 by lottavi           #+#    #+#             */
-/*   Updated: 2024/07/05 14:19:25 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/07/05 14:27:03 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ char	**get_args(char *input, char **args)
 
 char	*get_input(char *buffer)
 {
-	buffer = readline("minishell>");
+	buffer = readline("minishell> ");
 	if(NULL == buffer)
 	{
 		printf("exit\n");
