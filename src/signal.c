8b8e19bb0df8fb547d5cd9349b18d143d9150239
Leonardo Ottavi <leonardottavi02@gmail.com>
2024-07-06 12:41:46 +0200

    Fix signal handling in sigint_handler function

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:00:36 by lottavi           #+#    #+#             */
-/*   Updated: 2024/07/05 15:02:43 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/07/06 12:41:09 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,18 @@
 
 void	sigint_handler(int signum __attribute__((unused))) //ctrlc
 {
-	rl_replace_line("", 0);
-	printf("\nminishell> ");
+	if(g_path == NULL)
+	{
+		printf("\n");
+	}
+	else if(get_builtin_functions() != NULL)
+	{
+		printf("\n");
+	}
+	else
+	{
+		printf("\nminishell> ");
+	}
 }
 
-void	sigquit_handler(int signum __attribute__((unused))) // ctrl"\"
-{
-	exit(0);
-}
+
