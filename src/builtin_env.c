2022783c9ab4a578ecf776395c34d97384452dd0
lottavi <leonardottavi02@gmail.com>
2024-07-07 18:11:59 +0200

    unset added and work fine(strange but fine)

--- src/builtin_env.c
+++ src/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 17:06:13 by lottavi           #+#    #+#             */
-/*   Updated: 2024/07/06 18:21:23 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/07/07 18:10:49 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,29 @@ int	add_to_env(char *var)
 	return 0;
 }
 
+int	remove_from_env(char *var)
+{
+	int		i;
+
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		if (ft_strncmp(environ[i], var, ft_strlen(var)) == 0)
+		{
+			free(environ[i]);
+			while (environ[i + 1] != NULL)
+			{
+				environ[i] = environ[i + 1];
+				i++;
+			}
+			environ[i] = NULL;
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
+
 int	builtin_export(char **args)
 {
 	int	i;
@@ -52,20 +75,24 @@ int	builtin_export(char **args)
 	}
 	return (1);
 }
+
 int	builtin_unset(char **args)
 {
 	int	i;
 
 	if (args[1] == NULL)
 	{
-		printf("mini-shell: unset: a variable name is required\n");
+		printf("mini-shell: unset: not enough arguments\n");
 		return (1);
 	}
 	i = 1;
 	while (args[i] != NULL)
 	{
-		if (ft_unsetenv(args[i]) != 0)
-			perror("mini-shell");
+		if (remove_from_env(args[i]) != 0)
+		{
+			perror("mini-shell: export");
+			return (1);
+		}
 		i++;
 	}
 	return (1);
