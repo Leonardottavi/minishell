79f25c95df429aa1e76c3f8feb0b3bcc2b7bf193
Leonardo Ottavi <leonardottavi02@gmail.com>
2024-07-09 15:08:24 +0200

    Refactor execute function to improve code readability and performance

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 12:24:20 by lottavi           #+#    #+#             */
-/*   Updated: 2024/07/09 14:58:01 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/07/09 15:07:00 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,11 +110,6 @@ int	execute(char **args)
 	int	loop_status;
 
 	j = 0;
-	loop_status = execute_builtin(args);
-	if (loop_status != -1)
-	{
-		return (loop_status);
-	}
 	while (NULL != args[j])
 	{
 		if (0 == ft_strcmp(args[j], "|"))
@@ -122,6 +117,11 @@ int	execute(char **args)
 			return (execute_with_pipe(args));
 		}
 		j++;
+	}
+	loop_status = execute_builtin(args);
+	if (loop_status != -1)
+	{
+		return (loop_status);
 	}
 	return (execute_without_pipe(args));
 }
