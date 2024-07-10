287298f1b6a56f1ae186a5c4ba3ec54f9a4ff152
Leonardo Ottavi <leonardottavi02@gmail.com>
2024-07-10 14:09:11 +0200

    Refactor execute_with_pipe function

--- src/pipe.c
+++ src/pipe.c
@@ -5,102 +5,78 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/01 17:58:58 by lottavi           #+#    #+#             */
-/*   Updated: 2024/07/09 15:11:02 by lottavi          ###   ########.fr       */
+/*   Created: 2024/07/10 14:08:57 by lottavi           #+#    #+#             */
+/*   Updated: 2024/07/10 14:09:00 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 int execute_with_pipe(char **args) {
-    int num_pipes = 0;
-    int i;
-    int pipe_locations[64]; // Array to store locations of pipes
-    pipe_locations[0] = -1; // Initial value indicating no pipe before the first command
-
-    // Find pipe locations in args array
-    i = 0;
-    while (args[i]) {
-        if (strcmp(args[i], "|") == 0)
-        {
-            pipe_locations[++num_pipes] = i; // Store the index of pipe symbol
-            args[i] = NULL; // Replace "|" with NULL to mark end of arguments for execv
-        }
-        i++;
-    }
-
-    int num_commands = num_pipes + 1; // Number of commands to be executed
-    int pipes[num_pipes][2]; // Array to store pipes
-
-    // Create pipes
-    i=0;
-    while (i < num_pipes) {
-        if (pipe(pipes[i]) == -1) {
-            perror("pipe");
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-
-    // Execute commands using fork and execv
-    i = 0;
-    while (i < num_commands) {
-        pid_t pid;
-        if ((pid = fork()) == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pid == 0) {
-            // Child process
-
-            // Set up stdin redirection for all but first command
-            if (i > 0) {
-                if (dup2(pipes[i - 1][0], STDIN_FILENO) == -1) {
-                    perror("dup2");
-                    exit(EXIT_FAILURE);
-                }
-            }
-
-            // Set up stdout redirection for all but last command
-            if (i < num_pipes) {
-                if (dup2(pipes[i][1], STDOUT_FILENO) == -1) {
-                    perror("dup2");
-                    exit(EXIT_FAILURE);
-                }
-            }
-
-            // Close all pipe fds
-            int j = 0;
-            while (j < num_pipes) {
-                close(pipes[j][0]);
-                close(pipes[j][1]);
-                j++;
-            }
-
-            // Execute the command
+	int num_pipes = 0;
+	int i;
+	int pipe_locations[64];
+	pipe_locations[0] = -1;
+	i = 0;
+	while (args[i]) {
+		if (strcmp(args[i], "|") == 0) {
+			pipe_locations[++num_pipes] = i;
+			args[i] = NULL;
+		}
+		i++;
+	}
+	int num_commands = num_pipes + 1;
+	int pipes[num_pipes][2];
+	i=0;
+	while (i < num_pipes) {
+		if (pipe(pipes[i]) == -1) {
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < num_commands) {
+		pid_t pid;
+		if ((pid = fork()) == -1) {
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0) {
+			if (i > 0) {
+				if (dup2(pipes[i - 1][0], STDIN_FILENO) == -1) {
+					perror("dup2");
+					exit(EXIT_FAILURE);
+				}
+			}
+			if (i < num_pipes) {
+				if (dup2(pipes[i][1], STDOUT_FILENO) == -1) {
+					perror("dup2");
+					exit(EXIT_FAILURE);
+				}
+			}
+			int j = 0;
+			while (j < num_pipes) {
+				close(pipes[j][0]);
+				close(pipes[j][1]);
+				j++;
+			}
 			char *cmd_path = get_cmd_path(args[pipe_locations[i] + 1]);
 			execve(cmd_path, args + pipe_locations[i] + 1, environ);
 			perror("execve");
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-
-    // Close all pipe fds in parent process
-    i = 0;
-    while (i < num_pipes)
-    {
-        close(pipes[i][0]);
-        close(pipes[i][1]);
-        i++;
-    }
-
-
-    i=0;
-    while (i++ < num_commands)
-	{
-        wait(NULL);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < num_pipes) {
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+		i++;
+	}
+	i=0;
+	while (i++ < num_commands) {
+		wait(NULL);
 	}
 	return(1);
 }
